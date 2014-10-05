c63518aecb1f784f72ea96cc4c2a57dbfa7c76f9
StevenSerrien <steven.serrien@student.kdg.be>
2014-10-05 14:34:52 +0200

    Oplossing Arrays deel 1 toegevoegd.

--- opdracht-arrays-basis/opdracht-arrays-basis-deel1.php
+++ opdracht-arrays-basis/opdracht-arrays-basis-deel1.php
@@ -1,4 +1,27 @@
+<?php 
 
+$dieren = array('slang', 'koe', 'aap', 'olifant', 'neushoorn', 'beer', 'leeuw', 'slak', 'dolfijn', 'kwal');
+
+$dieren2[] = 'slang';
+$dieren2[] = 'koe';
+$dieren2[] = 'hond';
+$dieren2[] = 'kat';
+$dieren2[] = 'schildpad';
+$dieren2[] = 'leeuw';
+$dieren2[] = 'kameel';
+$dieren2[] = 'worm';
+$dieren2[] = 'schorpioen';
+$dieren2[] = 'kever';
+
+//Multidimensionale arrays
+
+$voertuig['landvoertuigen'] = array('vespa', 'fiets');
+$voertuig['watervoertuigen'] = array('surfplank', 'vlot', 'schoener', 'driemaster');
+$voertuig['luchtvoertuigen'] = array('luchtballon', 'helicopter', 'B52');
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,31 +30,7 @@
 	<body>
 
       <h1>Oplossing arrays basis: deel1</h1>
-
-      <pre><pre class='xdebug-var-dump' dir='ltr'>
-<b>array</b> <i>(size=8)</i>
-  0 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'kat'</font> <i>(length=3)</i>
-  1 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hond'</font> <i>(length=4)</i>
-  2 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'schaap'</font> <i>(length=6)</i>
-  3 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'slang'</font> <i>(length=5)</i>
-  4 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'kat'</font> <i>(length=3)</i>
-  5 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hond'</font> <i>(length=4)</i>
-  6 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'schaap'</font> <i>(length=6)</i>
-  7 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'slang'</font> <i>(length=5)</i>
-</pre></pre>
-		<pre><pre class='xdebug-var-dump' dir='ltr'>
-<b>array</b> <i>(size=3)</i>
-  'landvoertuigen' <font color='#888a85'>=&gt;</font> 
-    <b>array</b> <i>(size=2)</i>
-      0 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'jeep'</font> <i>(length=4)</i>
-      1 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'formule 1'</font> <i>(length=9)</i>
-  'watervoertuig' <font color='#888a85'>=&gt;</font> 
-    <b>array</b> <i>(size=2)</i>
-      0 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hoovercraft'</font> <i>(length=11)</i>
-      1 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'waterfiets'</font> <i>(length=10)</i>
-  'luchtvoertuig' <font color='#888a85'>=&gt;</font> 
-    <b>array</b> <i>(size=1)</i>
-      0 <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'Gulfstream G6'</font> <i>(length=13)</i>
-</pre></pre>
+      <?= var_dump($voertuig)?>
+   
 	</body>
 </html>
