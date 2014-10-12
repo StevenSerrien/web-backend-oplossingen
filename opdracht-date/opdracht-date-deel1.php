ab8abb32acea1669400370861cb5835e870ed3fb
StevenSerrien <steven.serrien@student.kdg.be>
2014-10-12 16:54:59 +0200

    Oplossing deel1 date

---
+++ opdracht-date/opdracht-date-deel1.php
@@ -0,0 +1,24 @@
+<?php 
+
+$timestamp = mktime(22, 35, 25, 01, 21, 1904);
+
+$date = strftime('%d %B %Y, %H:%M:%S %p', $timestamp);
+
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    	<title>Oplossing date: deel1</title>
+
+    </head>
+    <body>
+		
+		<h1>Oplossing date: deel1</h1>
+
+		<p>De timestamp van 22u 35m 25sec 21 januari 1904 is <?= $timestamp ?</p>
+
+		<p>De timestamp omgezet naar een datum: <?= $date ?></p>
+    </body>
+</html>
